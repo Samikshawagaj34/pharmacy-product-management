ce774af8c1d9d5f0b3d78a8898f8b72cf908e3fa
Rugvedi Wankhede <rugvediwankhede46@gmail.com>
2025-11-01 21:58:21 +0530

    added components on sign up page

---
+++ Frontend/src/pages/SignUp.jsx
@@ -0,0 +1,6 @@
+export default function ComponentName() { 
+    return (<div>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt dolore quae sint commodi consequuntur numquam? Neque velit harum, nisi nam eaque odit natus non, voluptate animi molestias quod modi possimus?</p>
+    </div>); 
+}
